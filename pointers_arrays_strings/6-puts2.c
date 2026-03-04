311ed383edd02d5856c5f6cda00226fed251a869
bothainakb <bothainakb@gmail.com>
2026-03-04 11:37:01 +0000

    6-puts2.c fix

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,17 +5,19 @@
  * puts2 - prints every other character of a string
  * @str: the string to print
  *
- * Description: Uses write instead of putchar.
+ * Description: Starts with the first character and prints
+ * every second character followed by a new line using write.
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-            write(1, &str[i], 1);
-        i++;
-    }
-    write(1, "\n", 1);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
 }
