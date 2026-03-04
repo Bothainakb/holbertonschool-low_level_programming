4e166bb1e9461f8f519885984e04ddd4e9914cc0
bothainakb <bothainakb@gmail.com>
2026-03-04 10:45:35 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: pointer to the string to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	while (i > 0)
+	{
+		i--;
+		write(1, &s[i], 1);
+	}
+
+	write(1, "\n", 1);
+}
