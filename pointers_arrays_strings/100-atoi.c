3988076e45dfbfaa0ee7ebae539ec1f168066324
bothainakb <bothainakb@gmail.com>
2026-03-04 12:11:25 +0000

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,36 +4,35 @@
  * _atoi - converts a string to an integer
  * @s: string to convert
  *
- * Return: the integer value represented by the string
+ * Return: integer value represented by the first number found
  */
 int _atoi(char *s)
 {
-	int i, sign, started;
-	unsigned int num;
+	int i, sign, num, digit_found;
 
 	i = 0;
 	sign = 1;
 	num = 0;
-	started = 0;
+	digit_found = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == '-' && !started)
+		if (s[i] == '-' && !digit_found)
 			sign *= -1;
-		else if (s[i] == '+' && !started)
+		else if (s[i] == '+' && !digit_found)
 			;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			started = 1;
-			num = num * 10 + (s[i] - '0');
+			digit_found = 1;
+			if (sign == -1)
+				num = num * 10 - (s[i] - '0');
+			else
+				num = num * 10 + (s[i] - '0');
 		}
-		else if (started)
+		else if (digit_found)
 			break;
 		i++;
 	}
 
-	/* Avoid negating INT_MIN by casting to int after unsigned accumulation */
-	if (sign == -1)
-		return (0 - (int)num);
-	return ((int)num);
+	return (num);
 }
