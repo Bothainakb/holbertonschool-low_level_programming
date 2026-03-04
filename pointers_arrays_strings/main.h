363e4a82f035533b018533215ab557d719078794
bothainakb <bothainakb@gmail.com>
2026-03-04 11:20:34 +0000

    main.h fix

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h> /* for write() */
+
+void _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void rev_string(char *s);
 
-#endif
+#endif /* MAIN_H */
