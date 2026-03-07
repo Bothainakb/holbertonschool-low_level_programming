1646d3ec5eb4296f85ebafe5b7dfed17a2a0dd97
bothainakb <bothainakb@gmail.com>
2026-03-07 17:46:19 +0000

    1-strncat.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,5 +16,6 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *leet(char *);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
