7b6a5aaeb4890e3229bbedd1387d83dec86886ed
bothainakb <bothainakb@gmail.com>
2026-03-09 10:20:20 +0000

    1-memcpy.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,5 +22,8 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
