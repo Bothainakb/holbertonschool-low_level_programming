52a42993a6c71c36dd18f9be6ce9f265a9fa5d8f
bothainakb <bothainakb@gmail.com>
2026-04-04 16:01:24 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - creates a new dog
@@ -15,31 +14,44 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 	char *name_copy;
 	char *owner_copy;
+	int i;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
+	/* allocate dog struct */
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	name_copy = malloc(strlen(name) + 1);
+	/* allocate and copy name */
+	for (i = 0; name[i]; i++)
+		;
+	name_copy = malloc(i + 1);
 	if (name_copy == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	strcpy(name_copy, name);
+	for (i = 0; name[i]; i++)
+		name_copy[i] = name[i];
+	name_copy[i] = '\0';
 
-	owner_copy = malloc(strlen(owner) + 1);
+	/* allocate and copy owner */
+	for (i = 0; owner[i]; i++)
+		;
+	owner_copy = malloc(i + 1);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
 		free(dog);
 		return (NULL);
 	}
-	strcpy(owner_copy, owner);
+	for (i = 0; owner[i]; i++)
+		owner_copy[i] = owner[i];
+	owner_copy[i] = '\0';
 
+	/* assign values */
 	dog->name = name_copy;
 	dog->age = age;
 	dog->owner = owner_copy;
