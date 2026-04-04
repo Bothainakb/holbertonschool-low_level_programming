6f3833cafbbcd5322f2bdb263cc5cd409090042f
bothainakb <bothainakb@gmail.com>
2026-04-04 15:54:21 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,6 +14,9 @@ struct dog
 	char *owner;
 };
 
+/* new type name dog_t for struct dog */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
