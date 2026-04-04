82f0b72c2d507ea7fa2e7280ba81faa609433cc4
bothainakb <bothainakb@gmail.com>
2026-04-04 16:05:51 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,34 @@
 #include <stdlib.h>
 
 /**
+ * _strdup - duplicates a string using malloc
+ * @str: string to duplicate
+ *
+ * Return: pointer to new string, or NULL on failure
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		;
+
+	copy = malloc(i + 1);
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		copy[i] = str[i];
+	copy[i] = '\0';
+
+	return (copy);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -12,49 +40,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *name_copy;
-	char *owner_copy;
-	int i;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	/* allocate dog struct */
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	/* allocate and copy name */
-	for (i = 0; name[i]; i++)
-		;
-	name_copy = malloc(i + 1);
-	if (name_copy == NULL)
+	dog->name = _strdup(name);
+	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; name[i]; i++)
-		name_copy[i] = name[i];
-	name_copy[i] = '\0';
 
-	/* allocate and copy owner */
-	for (i = 0; owner[i]; i++)
-		;
-	owner_copy = malloc(i + 1);
-	if (owner_copy == NULL)
+	dog->owner = _strdup(owner);
+	if (dog->owner == NULL)
 	{
-		free(name_copy);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; owner[i]; i++)
-		owner_copy[i] = owner[i];
-	owner_copy[i] = '\0';
 
-	/* assign values */
-	dog->name = name_copy;
 	dog->age = age;
-	dog->owner = owner_copy;
 
 	return (dog);
 }
