9f8d8dc6e9da539f7aac3b6da50e045d0834cd06
bothainakb <bothainakb@gmail.com>
2026-04-06 09:06:14 +0000

    3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,7 +1,11 @@
 #include "3-calc.h"
 
 /**
- * op_add - returns the sum of a and b
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
  */
 int op_add(int a, int b)
 {
@@ -9,7 +13,11 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - returns the difference of a and b
+ * op_sub - subtracts one integer from another
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
  */
 int op_sub(int a, int b)
 {
@@ -17,7 +25,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - returns the product of a and b
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
@@ -25,7 +37,11 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - returns the division of a by b
+ * op_div - divides one integer by another
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of division of a by b
  */
 int op_div(int a, int b)
 {
@@ -33,7 +49,11 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - returns the remainder of division of a by b
+ * op_mod - finds the remainder of division of one integer by another
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of division of a by b
  */
 int op_mod(int a, int b)
 {
