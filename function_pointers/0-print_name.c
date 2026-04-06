c47c03e62e39c8352ab7a8e5d3f975941ca0d94f
bothainakb <bothainakb@gmail.com>
2026-04-06 08:48:24 +0000

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name to be printed
+ * @f: function pointer to print the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
