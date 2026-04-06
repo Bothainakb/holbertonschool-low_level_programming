640b9628b8fb06b5adad39df5a0ef64ccc70faca
bothainakb <bothainakb@gmail.com>
2026-04-06 09:55:45 +0000

    100-main_opcodes.c

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0 on success, exits with 1 or 2 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i, n;
+	unsigned char *p = (unsigned char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", p[i]);
+		if (i != n - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
