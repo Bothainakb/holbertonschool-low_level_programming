cfeef5e49f2ee2aec3e30a2ba5f8726897f0161b
bothainakb <bothainakb@gmail.com>
2026-04-22 10:12:32 +0000

    3-cp.c

---
+++ file_io/3-cp.c
@@ -0,0 +1,89 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - copies content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success (never returns on failure)
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to, r, w, c_from, c_to;
+	char *buffer;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = malloc(BUFFER_SIZE);
+	if (buffer == NULL)
+		exit(99);
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		free(buffer);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		free(buffer);
+		close(fd_from);
+		exit(99);
+	}
+
+	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w != r)
+		{
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			free(buffer);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		free(buffer);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	c_from = close(fd_from);
+	if (c_from == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		free(buffer);
+		close(fd_to);
+		exit(100);
+	}
+
+	c_to = close(fd_to);
+	if (c_to == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		free(buffer);
+		exit(100);
+	}
+
+	free(buffer);
+	return (0);
+}
