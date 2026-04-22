ccfd2132d9cc4d95df179d1768b43d39f6e05034
bothainakb <bothainakb@gmail.com>
2026-04-22 10:17:39 +0000

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,17 +6,20 @@
 
 #define BUFFER_SIZE 1024
 
+int open_files(int *fd_from, int *fd_to, char **av);
+void close_fds(int fd_from, int fd_to, char *buffer);
+
 /**
  * main - copies file_from to file_to
  * @ac: argument count
  * @av: argument vector
  *
- * Return: 0 on success, exits with codes 97-100 on failure
+ * Return: 0 on success
  */
 int main(int ac, char **av)
 {
 	int fd_from, fd_to;
-	ssize_t r, w, c_from, c_to;
+	ssize_t r, w;
 	char *buffer;
 
 	if (ac != 3)
@@ -29,21 +32,10 @@ int main(int ac, char **av)
 	if (buffer == NULL)
 		exit(99);
 
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
+	if (open_files(&fd_from, &fd_to, av) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		free(buffer);
 		exit(98);
-	}
-
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		free(buffer);
-		close(fd_from);
-		exit(99);
 	}
 
 	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
@@ -52,9 +44,7 @@ int main(int ac, char **av)
 		if (w != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			free(buffer);
-			close(fd_from);
-			close(fd_to);
+			close_fds(fd_from, fd_to, buffer);
 			exit(99);
 		}
 	}
@@ -62,29 +52,59 @@ int main(int ac, char **av)
 	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		free(buffer);
-		close(fd_from);
-		close(fd_to);
+		close_fds(fd_from, fd_to, buffer);
 		exit(98);
 	}
 
-	c_from = close(fd_from);
-	if (c_from == -1)
+	close_fds(fd_from, fd_to, buffer);
+	return (0);
+}
+
+/**
+ * open_files - opens source and destination files
+ * @fd_from: pointer to source fd
+ * @fd_to: pointer to destination fd
+ * @av: arguments
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int open_files(int *fd_from, int *fd_to, char **av)
+{
+	*fd_from = open(av[1], O_RDONLY);
+	if (*fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		free(buffer);
-		close(fd_to);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		return (-1);
 	}
 
-	c_to = close(fd_to);
-	if (c_to == -1)
+	*fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (*fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		free(buffer);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		close(*fd_from);
+		return (-1);
 	}
+
+	return (0);
+}
+
+/**
+ * close_fds - closes files and frees buffer
+ * @fd_from: source fd
+ * @fd_to: destination fd
+ * @buffer: allocated buffer
+ */
+void close_fds(int fd_from, int fd_to, char *buffer)
+{
+	int c;
 
 	free(buffer);
-	return (0);
+
+	c = close(fd_from);
+	if (c == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+
+	c = close(fd_to);
+	if (c == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 } 
