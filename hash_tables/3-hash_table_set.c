2a2613cc5eba7dd91e36f8d27b2477ffb873f5d5
bothainakb <bothainakb@gmail.com>
2026-04-27 09:51:02 +0000

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,6 +2,58 @@
 #include <string.h>
 
 /**
+ * update_node - updates value if key exists
+ * @node: linked list node
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if updated, 0 if not found
+ */
+int update_node(hash_node_t *node, const char *key, const char *value)
+{
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (node->value != NULL);
+		}
+		node = node->next;
+	}
+	return (0);
+}
+
+/**
+ * create_node - creates a new node
+ * @key: key
+ * @value: value
+ *
+ * Return: pointer to node or NULL
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+
+	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
+		return (NULL);
+	}
+
+	return (node);
+}
+
+/**
  * hash_table_set - adds element to hash table
  * @ht: hash table
  * @key: key
@@ -14,41 +66,19 @@ int hash_table_set(hash_table_t *ht, const char *key,
 {
 	unsigned long int index;
 	hash_node_t *node;
-	char *val_copy;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	node = ht->array[index];
-	while (node)
-	{
-		if (strcmp(node->key, key) == 0)
-		{
-			free(node->value);
-			node->value = strdup(value);
-			return (node->value != NULL);
-		}
-		node = node->next;
-	}
+	if (update_node(ht->array[index], key, value))
+		return (1);
 
-	node = malloc(sizeof(hash_node_t));
+	node = create_node(key, value);
 	if (node == NULL)
 		return (0);
 
-	node->key = strdup(key);
-	val_copy = strdup(value);
-
-	if (node->key == NULL || val_copy == NULL)
-	{
-		free(node->key);
-		free(val_copy);
-		free(node);
-		return (0);
-	}
-
-	node->value = val_copy;
 	node->next = ht->array[index];
 	ht->array[index] = node;
 
